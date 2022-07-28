8670cd6c2e3024f3ea776c25a0447b03f054a81c
zcsryu2 <zcsryu2@126.com>
2022-07-28 16:20:25 +0800

    done with Collatz

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -5,7 +5,7 @@ import java.util.Observable;
 
 
 /** The state of a game of 2048.
- *  @author TODO: YOUR NAME HERE
+ *  @author TODO: YOUR NAME HERE Wang Xun
  */
 public class Model extends Observable {
     /** Current contents of the board. */
@@ -138,6 +138,7 @@ public class Model extends Observable {
      * */
     public static boolean emptySpaceExists(Board b) {
         // TODO: Fill in this function.
+
         return false;
     }
 
