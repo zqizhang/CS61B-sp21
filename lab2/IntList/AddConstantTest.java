67aeb2a2cb4c3f7562473c994e9b5839605acea5
wangxun <zcsryu2@126.com>
2024-07-28 20:54:02 +0800

    lab2 modify

--- lab2/IntList/AddConstantTest.java
+++ lab2/IntList/AddConstantTest.java
@@ -25,4 +25,10 @@ public class AddConstantTest {
         IntListExercises.addConstant(lst, 10);
         assertEquals("11 -> 12 -> 13 -> 14 -> 15 -> 16 -> 17 -> 18 -> 19", lst.toString());
     }
+    @Test
+    public void testAddConstantOne_1() {
+        IntList lst = IntList.of(1, 2, 3, 4, 5);
+        IntListExercises.addConstant(lst, -1);
+        assertEquals("0 -> 1 -> 2 -> 3 -> 4", lst.toString());
+    }
 }
